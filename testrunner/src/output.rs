ec14188be80114fecf709eec64f1ae60528b8fa9
Rain <rain1@fb.com>
2021-02-26 17:19:05 -0800

    [ci] fix clippy

--- testrunner/src/output.rs
+++ testrunner/src/output.rs
@@ -1,6 +1,9 @@
 // Copyright (c) The diem-devtools Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+// clippy complains about the Arbitrary impl for OutputFormat
+#![allow(clippy::unit_arg)]
+
 use anyhow::{bail, Context, Result};
 use serde::Serialize;
 use std::{fmt, io, str::FromStr};
