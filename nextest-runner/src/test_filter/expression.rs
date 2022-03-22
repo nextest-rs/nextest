a2b398336c2e3bf508f98829ae03e71430124868
Guilhem Vallat <guilhem.vallat@gmail.com>
2022-03-22 17:46:49 +0100

    Implement expression filtering: deps and rdeps

--- nextest-runner/src/test_filter/expression.rs
+++ nextest-runner/src/test_filter/expression.rs
@@ -1,10 +1,19 @@
 // Copyright (c) The nextest Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use std::collections::HashSet;
+
+use guppy::{
+    graph::{DependsCache, PackageGraph, PackageMetadata},
+    PackageId,
+};
+
 use crate::{
     errors::ParseFilterExprError, list::RustTestArtifact,
     test_filter::expression_parsing::parse_expression,
 };
+
+use super::expression_parsing::{Expr, SetDef};
 
 /// Matcher for name
 ///
@@ -37,13 +46,9 @@ impl Eq for NameMatcher {}
 /// Define a set of tests
 #[cfg_attr(test, derive(PartialEq, Eq))]
 #[derive(Debug, Clone)]
-pub enum SetDef {
-    /// All tests in a package
-    Package(NameMatcher),
-    /// All tests in a package dependencies
-    Deps(NameMatcher),
-    /// All tests in a package reverse dependencies
-    Rdeps(NameMatcher),
+pub enum FilteringSet {
+    /// All tests in packages
+    Packages(HashSet<PackageId>),
     /// All tests matching a name
     Test(NameMatcher),
     /// All tests
@@ -58,15 +63,15 @@ pub enum SetDef {
 /// Used to filter tests to run.
 #[cfg_attr(test, derive(PartialEq, Eq))]
 #[derive(Debug, Clone)]
-pub enum Expr {
+pub enum FilteringExpr {
     /// Accepts every tests not in the given expression
-    Not(Box<Expr>),
+    Not(Box<FilteringExpr>),
     /// Accepts every tests in either given expression
-    Union(Box<Expr>, Box<Expr>),
+    Union(Box<FilteringExpr>, Box<FilteringExpr>),
     /// Accepts every tests in both given expression
-    Intersection(Box<Expr>, Box<Expr>),
+    Intersection(Box<FilteringExpr>, Box<FilteringExpr>),
     /// Accepts every tests in a set
-    Set(SetDef),
+    Set(FilteringSet),
 }
 
 impl NameMatcher {
@@ -79,28 +84,130 @@ impl NameMatcher {
     }
 }
 
-impl SetDef {
+impl FilteringSet {
     fn includes(&self, artifact: &RustTestArtifact<'_>, name: &str) -> bool {
         match self {
             Self::All => true,
             Self::None => false,
             Self::Test(matcher) => matcher.is_match(name),
-            Self::Package(matcher) => matcher.is_match(artifact.package.name()),
-            Self::Deps(_matcher) => unimplemented!("deps not implemented"),
-            Self::Rdeps(_matcher) => unimplemented!("rdeps not implemented"),
+            Self::Packages(packages) => packages.contains(artifact.package.id()),
         }
     }
 }
 
-impl Expr {
-    pub fn parse(input: &str) -> Result<Expr, ParseFilterExprError> {
+fn matching_packages(
+    matcher: &NameMatcher,
+    all_packages: &[PackageMetadata],
+) -> HashSet<PackageId> {
+    all_packages
+        .iter()
+        .filter(|p| matcher.is_match(p.name()))
+        .map(|p| p.id().clone())
+        .collect()
+}
+
+fn dependencies_packages(
+    matcher: &NameMatcher,
+    all_packages: &[PackageMetadata],
+    cache: &mut DependsCache,
+) -> HashSet<PackageId> {
+    let packages = all_packages
+        .iter()
+        .filter(|p| matcher.is_match(p.name()))
+        .map(|p| p.id());
+    let mut set = HashSet::new();
+    for id1 in packages {
+        for p2 in all_packages {
+            let id2 = p2.id();
+            if id1 == id2 {
+                continue;
+            }
+            if cache.depends_on(id1, id2).unwrap_or(false) {
+                set.insert(id2.clone());
+            }
+        }
+    }
+    set
+}
+
+fn rdependencies_packages(
+    matcher: &NameMatcher,
+    all_packages: &[PackageMetadata],
+    cache: &mut DependsCache,
+) -> HashSet<PackageId> {
+    let packages = all_packages
+        .iter()
+        .filter(|p| matcher.is_match(p.name()))
+        .map(|p| p.id());
+    let mut set = HashSet::new();
+    for id1 in packages {
+        for p2 in all_packages {
+            let id2 = p2.id();
+            if id1 == id2 {
+                continue;
+            }
+            if cache.depends_on(id2, id1).unwrap_or(false) {
+                set.insert(id2.clone());
+            }
+        }
+    }
+    set
+}
+
+fn compile_set_def(
+    set: &SetDef,
+    packages: &[PackageMetadata],
+    cache: &mut DependsCache,
+) -> FilteringSet {
+    match set {
+        SetDef::Package(matcher) => FilteringSet::Packages(matching_packages(matcher, packages)),
+        SetDef::Deps(matcher) => {
+            FilteringSet::Packages(dependencies_packages(matcher, packages, cache))
+        }
+        SetDef::Rdeps(matcher) => {
+            FilteringSet::Packages(rdependencies_packages(matcher, packages, cache))
+        }
+        SetDef::Test(matcher) => FilteringSet::Test(matcher.clone()),
+        SetDef::All => FilteringSet::All,
+        SetDef::None => FilteringSet::None,
+    }
+}
+
+fn compile_expr(
+    expr: &Expr,
+    packages: &[PackageMetadata],
+    cache: &mut DependsCache,
+) -> FilteringExpr {
+    match expr {
+        Expr::Set(set) => FilteringExpr::Set(compile_set_def(set, packages, cache)),
+        Expr::Not(expr) => FilteringExpr::Not(Box::new(compile_expr(expr, packages, cache))),
+        Expr::Union(expr_1, expr_2) => FilteringExpr::Union(
+            Box::new(compile_expr(expr_1, packages, cache)),
+            Box::new(compile_expr(expr_2, packages, cache)),
+        ),
+        Expr::Intersection(expr_1, expr_2) => FilteringExpr::Intersection(
+            Box::new(compile_expr(expr_1, packages, cache)),
+            Box::new(compile_expr(expr_2, packages, cache)),
+        ),
+    }
+}
+
+impl FilteringExpr {
+    /// Parse a filtering expression
+    pub fn parse(input: &str, graph: &PackageGraph) -> Result<FilteringExpr, ParseFilterExprError> {
         let info = nom_tracable::TracableInfo::new();
         match parse_expression(super::expression_parsing::Span::new_extra(input, info)) {
-            Ok(expr) => Ok(expr),
+            Ok(expr) => {
+                let in_workspace_packages: Vec<_> =
+                    graph.packages().filter(|p| p.in_workspace()).collect();
+                let mut cache = graph.new_depends_cache();
+                Ok(compile_expr(&expr, &in_workspace_packages, &mut cache))
+            }
             Err(_) => Err(ParseFilterExprError::Failed(input.to_string())),
         }
     }
 
+    /// Returns true if the given test is accepted by this filter
     pub fn includes(&self, artifact: &RustTestArtifact<'_>, name: &str) -> bool {
         match self {
             Self::Set(set) => set.includes(artifact, name),
