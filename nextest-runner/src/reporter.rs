b107dfcb8f9d043b38bd60ae62e5ebd58ad77dca
Rain <rain@sunshowers.io>
2022-03-21 19:39:51 -0700

    [nextest-runner] fix clippy

--- nextest-runner/src/reporter.rs
+++ nextest-runner/src/reporter.rs
@@ -218,7 +218,7 @@ impl TestReporterBuilder {
         let styles = Box::new(Styles::default());
         let binary_id_width = test_list
             .iter()
-            .filter_map(|(_, info)| (info.testcases.len() > 0).then(|| info.binary_id.len()))
+            .filter_map(|(_, info)| (!info.testcases.is_empty()).then(|| info.binary_id.len()))
             .max()
             .unwrap_or_default();
         let aggregator = EventAggregator::new(profile);
