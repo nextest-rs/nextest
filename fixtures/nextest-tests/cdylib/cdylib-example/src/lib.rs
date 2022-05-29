5ed67b6919952bce5c66b6ae89709dee3851f023
Rain <rain@sunshowers.io>
2022-05-29 16:42:19 -0700

    [fixtures] add a test within cdylib-example

--- fixtures/nextest-tests/cdylib/cdylib-example/src/lib.rs
+++ fixtures/nextest-tests/cdylib/cdylib-example/src/lib.rs
@@ -5,3 +5,13 @@
 pub extern "C" fn multiply_two(a: i32, b: i32) -> i32 {
     a * b
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiply_two_cdylib() {
+        assert_eq!(multiply_two(5, 6), 30);
+    }
+}
