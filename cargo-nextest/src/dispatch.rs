ee12fb1aa4c664a3c5d04f3adaaad91d56204b9d
jiangying <krivergo3@gmail.com>
2022-09-12 14:00:19 +0800

    [doc] add [test-threads default] tip to run-help.txt (#518)

--- cargo-nextest/src/dispatch.rs
+++ cargo-nextest/src/dispatch.rs
@@ -600,6 +600,7 @@ pub struct TestRunnerOpts {
     no_run: bool,
 
     /// Number of tests to run simultaneously [possible values: integer or "num-cpus"]
+    /// [default: from profile]
     #[clap(
         long,
         short = 'j',
