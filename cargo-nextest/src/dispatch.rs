8e8f3b073a8603d3d2d7b061b3afa82c328b26da
jiangying <krivergo3@gmail.com>
2022-09-22 11:09:40 +0800

    update doc

--- cargo-nextest/src/dispatch.rs
+++ cargo-nextest/src/dispatch.rs
@@ -600,7 +600,8 @@ pub struct TestRunnerOpts {
     no_run: bool,
 
     /// Number of tests to run simultaneously 
-    /// [possible values: integer or "num-cpus"] [default: from profile]
+    /// [possible values: integer or "num-cpus"]
+    /// [default: from profile]
     #[clap(
         long,
         short = 'j',
