4f656df4f8843ed00003517d5e093cc19dbe73ff
Rain <rain@sunshowers.io>
2022-11-22 21:28:29 -0800

    [release] verify crates before releasing them
    Since the published Cargo.lock will contain newer versions of
    dependencies anyway, ensure that crates build before releasing them.

--- scripts/cargo-release-publish.sh
+++ scripts/cargo-release-publish.sh
@@ -9,10 +9,8 @@ set -xe -o pipefail
 git checkout -B to-release
 
 # --execute: actually does the release
-# --no-verify: doesn't build before releasing (this is because the cargo publish process might pull
-# in new versions of dependencies, which might have regressions)
 # --no-confirm: don't ask for confirmation, since this is a non-interactive script
-cargo release publish --publish --execute --no-verify --no-confirm --workspace "$@"
+cargo release publish --publish --execute --no-confirm --workspace "$@"
 
 git checkout -
 git branch -D to-release
