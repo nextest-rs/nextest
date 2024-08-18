2ebbead0503e8f96a228ed9bc42ce05c9c292366
Rain <rain@sunshowers.io>
2024-08-17 20:27:16 -0700

    [meta] update renovate post-upgrade script
    Flagged by outdated cargo-hakari:
    https://github.com/nextest-rs/nextest/pull/1642#issuecomment-2295094296.

--- scripts/renovate-post-upgrade.sh
+++ scripts/renovate-post-upgrade.sh
@@ -2,11 +2,48 @@
 
 set -euo pipefail
 
-# Fetch cargo binstall.
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+# Function to retry a command up to 3 times.
+function retry_command {
+    local retries=3
+    local delay=5
+    local count=0
+    until "$@"; do
+        exit_code=$?
+        count=$((count+1))
+        if [ $count -lt $retries ]; then
+            echo "Command failed with exit code $exit_code. Retrying in $delay seconds..."
+            sleep $delay
+        else
+            echo "Command failed with exit code $exit_code after $count attempts."
+            return $exit_code
+        fi
+    done
+}
 
-# Install cargo-hakari.
-cargo binstall cargo-hakari@0.9.27 --no-confirm
+# If cargo isn't present, skip this -- it implies that a non-Rust dependency was
+# updated.
+if ! command -v cargo &> /dev/null; then
+    echo "Skipping cargo-hakari update because cargo is not present."
+    exit 0
+fi
 
-# Run cargo hakari.
+# Download and install cargo-hakari if it is not already installed.
+if ! command -v cargo-hakari &> /dev/null; then
+    # Need cargo-binstall to install cargo-hakari.
+    if ! command -v cargo-binstall &> /dev/null; then
+        # Fetch cargo binstall.
+        echo "Installing cargo-binstall..."
+        tempdir=$(mktemp -d)
+        curl --retry 3 -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh -o "$tempdir"/install-from-binstall-release.sh
+        retry_command bash "$tempdir"/install-from-binstall-release.sh
+        rm -rf "$tempdir"
+    fi
+
+    # Install cargo-hakari.
+    echo "Installing cargo-hakari..."
+    retry_command cargo binstall cargo-hakari --no-confirm
+fi
+
+# Run cargo hakari to regenerate the workspace-hack file.
+echo "Running cargo-hakari..."
 cargo hakari generate
