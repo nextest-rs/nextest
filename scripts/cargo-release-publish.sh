8211710fbecce9ebdd72ffafcc8d6995da8cf44a
Rain <rain@sunshowers.io>
2024-09-25 17:51:25 -0700

    [cargo-nextest] improve -V/--version
    Add version info to crate releases, and roughly match what Cargo does. (We
    follow the clap default where `--version` is the long/verbose version).

--- scripts/cargo-release-publish.sh
+++ scripts/cargo-release-publish.sh
@@ -8,9 +8,25 @@ set -xe -o pipefail
 # Check out this branch, creating it if it doesn't exist.
 git checkout -B to-release
 
-# --execute: actually does the release
-# --no-confirm: don't ask for confirmation, since this is a non-interactive script
-cargo release publish --publish --execute --no-confirm --workspace "$@"
+# Publish all crates except cargo-nextest first. Do this against main so `.cargo_vcs_info.json` is
+# valid. (cargo-nextest is the only crate that cares about commit info.)
+cargo release publish --publish --execute --no-confirm --workspace --exclude cargo-nextest
+
+if [[ $PUBLISH_CARGO_NEXTEST == "1" ]]; then
+    # Write out commit-related metadata. This matches cargo-nextest's build.rs.
+    git log -1 --date=short --format="%H %h %cd" --abbrev=9 > cargo-nextest/nextest-commit-info
+
+    # Making a commit here is important because cargo-release does not allow passing in
+    # --allow-dirty. But note that `nextest-commit-info` is what's on main.
+    #
+    # This does unfortunately mean that Cargo's own `.cargo_vcs_info.json` will be incorrect, but
+    # what can you do.
+    git add cargo-nextest/nextest-commit-info
+    git commit -m "Write out commit info for cargo-nextest"
+
+    # Publish cargo-nextest.
+    cargo release publish --publish --execute --no-confirm -p cargo-nextest
+fi
 
 git checkout -
 git branch -D to-release
