3b72d41ada1ddae5fec98b3d37ed7d5b66618fe6
Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>
2025-07-11 13:36:35 +0200

    build-site workaround for macOS

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -4,4 +4,8 @@ set -e -o pipefail
 
 # Build the site with mkdocs
 cd site
+
+# If cairosvg can't find cairo library: https://t.ly/MfX6u
+command -v brew > /dev/null && export DYLD_FALLBACK_LIBRARY_PATH="$(brew --prefix)/lib"
+
 uv run mkdocs build
