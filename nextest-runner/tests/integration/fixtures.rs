b6529b1bed3c46cf3ef4c1b93d60fb73d8053d55
Rain <rain@sunshowers.io>
2026-06-05 15:38:26 -0700

    [integration-tests] restore coverage for cargo [env] matrix (#3385)
    We ended up accidentally dropping this coverage in #2861.

--- nextest-runner/tests/integration/fixtures.rs
+++ nextest-runner/tests/integration/fixtures.rs
@@ -15,44 +15,6 @@ pub(crate) fn test_init() {
     unsafe {
         std::env::set_var("RUSTFLAGS", "-C prefer-dynamic");
 
-        std::env::set_var(
-            "__NEXTEST_ENV_VAR_FOR_TESTING_IN_PARENT_ENV_NO_OVERRIDE",
-            "test-PASSED-value-set-by-environment",
-        );
-        std::env::set_var(
-            "__NEXTEST_ENV_VAR_FOR_TESTING_IN_PARENT_ENV_OVERRIDDEN",
-            "test-FAILED-value-set-by-environment",
-        );
-        std::env::set_var(
-            "__NEXTEST_ENV_VAR_FOR_TESTING_IN_PARENT_ENV_RELATIVE_NO_OVERRIDE",
-            "test-PASSED-value-set-by-environment",
-        );
-        std::env::set_var(
-            "__NEXTEST_ENV_VAR_FOR_TESTING_IN_PARENT_ENV_RELATIVE_OVERRIDDEN",
-            "test-FAILED-value-set-by-environment",
-        );
-
-        std::env::set_var(
-            "__NEXTEST_TESTING_EXTRA_CONFIG_OVERRIDE_FORCE_IN_EXTRA",
-            "test-FAILED-value-set-by-environment",
-        );
-        std::env::set_var(
-            "__NEXTEST_TESTING_EXTRA_CONFIG_OVERRIDE_FORCE_IN_MAIN",
-            "test-PASSED-value-set-by-environment",
-        );
-        std::env::set_var(
-            "__NEXTEST_TESTING_EXTRA_CONFIG_OVERRIDE_FORCE_IN_BOTH",
-            "test-FAILED-value-set-by-environment",
-        );
-        std::env::set_var(
-            "__NEXTEST_TESTING_EXTRA_CONFIG_OVERRIDE_FORCE_NONE",
-            "test-PASSED-value-set-by-environment",
-        );
-        std::env::set_var(
-            "__NEXTEST_TESTING_EXTRA_CONFIG_OVERRIDE_FORCE_FALSE",
-            "test-PASSED-value-set-by-environment",
-        );
-
         // Remove OUT_DIR from the environment, as it interferes with tests
         // (some of them expect that OUT_DIR isn't set.)
         std::env::remove_var("OUT_DIR");
