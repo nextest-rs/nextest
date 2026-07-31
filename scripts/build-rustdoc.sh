9a254758485e5cfbe7339105e0faf5fb2d234e55
Rain <rain@sunshowers.io>
2026-07-30 23:00:50 -0700

    [site] fix docs publishing (#3510)
    Looks like we had crossed the 20k file Cloudflare Pages threshold.

--- scripts/build-rustdoc.sh
+++ scripts/build-rustdoc.sh
@@ -2,9 +2,16 @@
 
 set -e -o pipefail
 
+# A denylist of crates to skip.
+#
+# libc emits ~14k HTML pages (one per constant), and is not a public dependency
+# of nextest, so we strip it.
+EXCLUDE_RE='^(libc)@'
+
 # Build docs for all crates and direct dependencies. The gawk script turns e.g. "quick-junit v0.1.0"
 # into "quick-junit@0.1.0".
 cargo tree --depth 1 -e normal --prefix none \
     | gawk '{ gsub(" v", "@", $0); printf("%s\n", $1); }' \
+    | grep -Ev "$EXCLUDE_RE" \
     | xargs printf -- '-p %s\n' \
     | xargs cargo doc --no-deps --lib
