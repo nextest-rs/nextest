e65de9e1a08bcadc0c85cec1fe26cfedaaa37ed8
Rain <rain@sunshowers.io>
2026-08-10 18:38:14 -0700

    [release] add retries to crate publishing
    Deal with races and transient crates.io errors.

--- scripts/cargo-release-publish.sh
+++ scripts/cargo-release-publish.sh
@@ -1,8 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Use cargo-release to publish crates to crates.io.
 
 set -xe -o pipefail
+
+# Publish a crate with retries.
+#
+# Publishing a crate can fail for at least two reasons:
+#
+# * concurrent release jobs can race and result in 400 "already uploaded".
+# * crates.io can return transient 5xx errors.
+#
+# This retry should hopefully catch and converge with both reasons.
+publish_with_retries() {
+    local attempt
+    for attempt in 1 2 3; do
+        if cargo release publish --publish --execute --no-confirm "$@"; then
+            return 0
+        fi
+        if [[ "${attempt}" -lt 3 ]]; then
+            sleep $((attempt * 15))
+        fi
+    done
+    echo "error: failed to publish after 3 attempts" >&2
+    return 1
+}
 
 # cargo-release requires a release off a branch (maybe it shouldn't?)
 # Check out this branch, creating it if it doesn't exist.
@@ -10,7 +32,7 @@ git checkout -B to-release
 
 # Publish all crates except cargo-nextest first. Do this against main so `.cargo_vcs_info.json` is
 # valid. (cargo-nextest is the only crate that cares about commit info.)
-cargo release publish --publish --execute --no-confirm --workspace --exclude cargo-nextest --exclude internal-test
+publish_with_retries --workspace --exclude cargo-nextest --exclude internal-test
 
 if [[ $PUBLISH_CARGO_NEXTEST == "1" ]]; then
     # Write out commit-related metadata. This matches cargo-nextest's build.rs.
@@ -28,7 +50,7 @@ if [[ $PUBLISH_CARGO_NEXTEST == "1" ]]; then
     git commit -m "Write out commit info for cargo-nextest"
 
     # Publish cargo-nextest.
-    cargo release publish --publish --execute --no-confirm -p cargo-nextest
+    publish_with_retries -p cargo-nextest
 fi
 
 git checkout -
